b3a6584904aec8cb322c1e1dae98847dbc6f13b0
feyilasisi <flasisi@cloudvantage.com.ng>
2025-12-10 20:23:15 +0100

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,17 +3,6 @@ module "network" {
 }
 
 
-# BLOCK TO BE COPIED WHEN A NEW EC2 INSTANCE IS NEEDED
-# "ansible_slave_agent" = {
-#       subnet_id     = module.network.public_subnets[0]
-#       security_id   = var.security_id #module.security.security_id
-#       server_name   = "ansible_slave_agent"
-#       key_name      = var.IaC_key
-#       ami           = var.ami_ubuntu
-#       instance_type = var.instance_type_t2-micro
-#       volume_size   = var.volume_size_12GB
-#     }
-
 module "ec2" {
   source = "./modules/ec2"
   for_each = {
@@ -27,26 +16,7 @@ module "ec2" {
       volume_size   = var.volume_size_12GB
     }
 
-    # "ansible_slave_agent" = {
-    #   subnet_id     = module.network.public_subnets[0]
-    #   security_id   = var.security_id #module.security.security_id
-    #   server_name   = "ansible_slave_agent"
-    #   key_name      = var.IaC_key
-    #   ami           = var.ami_ubuntu
-    #   instance_type = var.instance_type_t2-micro
-    #   volume_size   = var.volume_size_12GB
-    # }
 
-
-    # "terraform_test_server" = {
-    #   subnet_id     = module.network.public_subnets[0]
-    #   security_id   = var.security_id #module.security.security_id
-    #   server_name   = "terraform_test_server"
-    #   key_name      = var.IaC_key
-    #   ami           = var.ami_centos
-    #   instance_type = var.instance_type_t2-micro
-    #   volume_size   = var.volume_size_20GB
-    # }
   }
 
   ami           = each.value.ami
